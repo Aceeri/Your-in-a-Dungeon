2de2d22a31d497332c8819aeaa018d2899cc29a3
unknown <aceeristudios@gmail.com>
2015-05-12 17:51:58 -0700

    fixes

--- src/main/character/Player.java
+++ src/main/character/Player.java
@@ -1,13 +1,22 @@
 package main.character;
 
 import main.misc.Vector2;
+import main.object.Projectile;
 import main.Manager;
 
 public class Player extends main.object.Object {
-	public double health = 10;
-	public String name;
 	
-	public Player(Vector2 position) {
+	public double health = 10;
+	
+	public double cooldown = 0;
+	public double ability1Cooldown = 0;
+	public double ability2Cooldown = 0;
+	
+	public double cooldownTime = 1000;
+	public double ability1Time = 1000;
+	public double ability2Time = 1000;
+	
+	public Player(Class c, Vector2 position) {
 		super(position);
 		
 		Size = new Vector2(20, 20);
@@ -22,4 +31,17 @@ public class Player extends main.object.Object {
 		
 		setImage();
 	}
+	
+	public void step() {
+		super.step();
+		
+		cooldown = cooldown > 0 ? cooldown - 16 : 0;
+		ability1Cooldown = ability1Cooldown > 0 ? ability1Cooldown - 16 : 0;
+		ability2Cooldown = ability2Cooldown > 0 ? ability2Cooldown - 16 : 0;
+	}
+	
+	//override these with new abilities
+	public void ability1() { }
+	
+	public void ability2() { }
 }
